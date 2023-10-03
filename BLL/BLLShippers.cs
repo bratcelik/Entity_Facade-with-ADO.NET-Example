92c6d5cd7f24142f017d25c39591b96426172e44
Berat Sercan Çelik <bratcelik@gmail.com>
2023-10-03 10:47:03 +0300

    Business Logic Layer implemented

---
+++ BLL/BLLShippers.cs
@@ -0,0 +1,64 @@
+﻿using Entity;
+using Facade;
+
+namespace BLL
+{
+    public class BLLShippers
+    {
+        public static bool BLL_INSERT(EShipper shipper)
+        {
+            bool sonuc;
+            if(!string.IsNullOrEmpty(shipper.CompanyName) && !string.IsNullOrEmpty(shipper.Phone))
+            {
+                if (shipper.CompanyName.Length <= 40 && shipper.Phone.Length <= 24)
+                {
+                    sonuc = FShippers.Insert(shipper);
+                }
+                else
+                {
+                    sonuc =false;
+                }
+            }
+            else
+            {
+                sonuc =false;
+            }
+            return sonuc;
+        }
+
+        public static bool BLL_UPDATE(EShipper shipper)
+        {
+            bool sonuc;
+            if (shipper.ShipperID > 0 && !string.IsNullOrEmpty(shipper.CompanyName) && !string.IsNullOrEmpty(shipper.Phone))
+            {
+                if (shipper.CompanyName.Length <= 40 && shipper.Phone.Length <= 24)
+                {
+                    sonuc = FShippers.Update(shipper);
+                }
+                else
+                {
+                    sonuc = false;
+                }
+            }
+            else
+            {
+                sonuc = false;
+            }
+            return sonuc;
+        }
+
+        public static bool BLL_DELETE(int shipperID)
+        {
+            bool sonuc;
+            if (shipperID > 0)
+            {
+                sonuc = FShippers.Delete(shipperID);
+            }
+            else
+            {
+                sonuc = false;
+            }
+            return sonuc;
+        }
+    }
+}
